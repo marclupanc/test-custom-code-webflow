84bd2d99c4249df198aa812c90fe8ee6b6515d2e
Marc <43423591+marclupanc@users.noreply.github.com>
2024-10-03 16:55:52 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,13 +1,31 @@
+const Counter = () => {
+  //initilize a count variable at 0, the setCount function
+  // will be used to re-set the "count" value.
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h2>{count} likes</h2>
+      {/* Reset count to its previous value + 1 */}
+      <span onClick={() => setCount(count + 1)}>👍🏽</span>
+      {/* Reset count to its previous value - 1 */}
+      <span onClick={() => setCount(count - 1)}>👎🏽</span>
+      <h3>Like or dislike to increase/decrease</h3>
+    </div>
+  );
+};
+
 const App = () => {
-    const [store, setStore] = React.useState("hello");
-    console.log(store);
     return (
       <div>
         <h1>This is a Heading</h1>
         <p>This is a paragraph.</p>
+        <Counter />
       </div>
     );
   };
-  
+
+
+
+
   ReactDOM.render(<App />, document.getElementById("root"));
   
